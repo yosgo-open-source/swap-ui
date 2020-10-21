64d177a31e5ba64364df888cbf68d27dbc69ff9b
kcin1993 <kcin1993@gmail.com>
2020-10-21 17:13:11 +0800

    修正 Modal 次按鈕屬性、zindex、以及顯示特效

--- src/SWAPModal/SWAPModal.tsx
+++ src/SWAPModal/SWAPModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import {
   Container,
@@ -10,6 +10,7 @@ import {
   Divider,
   Button,
   Fade,
+  Slide,
 } from "@material-ui/core";
 
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
@@ -18,6 +19,26 @@ import WarningIcon from "@material-ui/icons/Warning";
 
 import { SWAPModalProps } from "./SWAPModal.types";
 import SWAPSpace from "../SWAPSpace/SWAPSpace";
+
+const ModalOpenEffect = (props: {
+  sm: boolean;
+  in: boolean;
+  children: React.ReactNode;
+}) => {
+  return (
+    <>
+      {props.sm ? (
+        <Slide in={props.in} direction="up" mountOnEnter unmountOnExit>
+          <div>{props.children}</div>
+        </Slide>
+      ) : (
+        <Fade in={props.in}>
+          <div>{props.children}</div>
+        </Fade>
+      )}
+    </>
+  );
+};
 
 const SWAPModal: React.FC<SWAPModalProps> = ({
   open,
@@ -32,6 +53,8 @@ const SWAPModal: React.FC<SWAPModalProps> = ({
   closeWindowOnSuccessMessage,
   reloadOnWindowClose,
 }) => {
+  const [sm, setSm] = useState(false);
+
   const handleModalSize = () => {
     const clientHeight = `${document.documentElement.clientHeight}px`;
     const wrap = document.getElementById("modal_wrap");
@@ -73,114 +96,127 @@ const SWAPModal: React.FC<SWAPModalProps> = ({
         modalCloseWindow();
       }, 1000);
     }
+    /**一開始偵測視窗大小，決定 modal 效果 */
+    const clientWidth = document.documentElement.clientWidth;
+    if (clientWidth < 960) {
+      setSm(true);
+    }
   }, [successMessage, errorMessage, children]);
 
   return (
-    <Modal open={open} onRendered={() => handleModalSize()}>
+    <Modal
+      open={open}
+      onRendered={() => handleModalSize()}
+      style={{ zIndex: 2147483647 }}
+    >
       <Fade in={open}>
         <SWAPModalWrap id="modal_wrap">
-          <Paper className="modal_inner" id="modal_inner">
-            <div className="modal_inner_header" id="modal_header">
-              <Container maxWidth="lg">
-                <SWAPSpace size="middle" />
-                <Grid
-                  container
-                  wrap="nowrap"
-                  alignItems="center"
-                  spacing={1}
-                  justify="space-between"
-                >
-                  <Grid item>
-                    <Typography variant="h5">{title}</Typography>
-                    {helpText ? (
-                      <Typography variant="body2" color="textSecondary">
-                        {helpText}
-                      </Typography>
-                    ) : null}
+          <ModalOpenEffect in={open} sm={sm}>
+            <Paper className="modal_inner" id="modal_inner">
+              <div className="modal_inner_header" id="modal_header">
+                <Container maxWidth="lg">
+                  <SWAPSpace size="middle" />
+                  <Grid
+                    container
+                    wrap="nowrap"
+                    alignItems="center"
+                    spacing={1}
+                    justify="space-between"
+                  >
+                    <Grid item>
+                      <Typography variant="h5">{title}</Typography>
+                      {helpText ? (
+                        <Typography variant="body2" color="textSecondary">
+                          {helpText}
+                        </Typography>
+                      ) : null}
+                    </Grid>
+                    <Grid item>
+                      <IconButton onClick={() => modalCloseWindow()}>
+                        <CloseIcon fontSize="large" />
+                      </IconButton>
+                    </Grid>
                   </Grid>
-                  <Grid item>
-                    <IconButton onClick={() => modalCloseWindow()}>
-                      <CloseIcon fontSize="large" />
-                    </IconButton>
-                  </Grid>
-                </Grid>
-                <SWAPSpace size="middle" />
-              </Container>
-              <Divider />
-            </div>
-            <div className="modal_inner_body" id="modal_body">
-              <Container maxWidth="lg">
-                <SWAPSpace size="middle" />
-                <div>{children}</div>
-                <SWAPSpace size="middle" />
-              </Container>
-            </div>
-            <div className="modal_inner_bottom" id="modal_bottom">
-              <Divider />
-              <Container maxWidth="lg">
-                <SWAPSpace size="middle" />
-                {(successMessage && successMessage.length > 0) ||
-                (errorMessage && errorMessage.length > 0) ? (
-                  <Fade in>
-                    <div>
-                      <Grid
-                        container
-                        wrap="nowrap"
-                        alignItems="center"
-                        justify="flex-end"
-                      >
-                        <Grid item style={{ margin: "0 6px -6px 0" }}>
-                          {successMessage ? (
-                            <CheckCircleIcon color="primary" />
-                          ) : (
-                            <WarningIcon color="secondary" />
-                          )}
+                  <SWAPSpace size="middle" />
+                </Container>
+                <Divider />
+              </div>
+              <div className="modal_inner_body" id="modal_body">
+                <Container maxWidth="lg">
+                  <SWAPSpace size="middle" />
+                  <div>{children}</div>
+                  <SWAPSpace size="middle" />
+                </Container>
+              </div>
+              <div className="modal_inner_bottom" id="modal_bottom">
+                <Divider />
+                <Container maxWidth="lg">
+                  <SWAPSpace size="middle" />
+                  {(successMessage && successMessage.length > 0) ||
+                  (errorMessage && errorMessage.length > 0) ? (
+                    <Fade in>
+                      <div>
+                        <Grid
+                          container
+                          wrap="nowrap"
+                          alignItems="center"
+                          justify="flex-end"
+                        >
+                          <Grid item style={{ margin: "0 6px -6px 0" }}>
+                            {successMessage ? (
+                              <CheckCircleIcon color="primary" />
+                            ) : (
+                              <WarningIcon color="secondary" />
+                            )}
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="body1">
+                              {successMessage ? successMessage : errorMessage}
+                            </Typography>
+                          </Grid>
                         </Grid>
-                        <Grid item>
-                          <Typography variant="body1">
-                            {successMessage ? successMessage : errorMessage}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                      <SWAPSpace size="small" />
-                    </div>
-                  </Fade>
-                ) : null}
-                <Grid
-                  container
-                  wrap="nowrap"
-                  alignItems="center"
-                  spacing={1}
-                  justify="flex-end"
-                >
-                  <Grid item>
-                    {secondaryButton && secondaryButton.title ? (
+                        <SWAPSpace size="small" />
+                      </div>
+                    </Fade>
+                  ) : null}
+                  <Grid
+                    container
+                    wrap="nowrap"
+                    alignItems="center"
+                    spacing={1}
+                    justify="flex-end"
+                  >
+                    <Grid item>
+                      {secondaryButton &&
+                      secondaryButton.title &&
+                      secondaryButton.title.length > 0 ? (
+                        <Button
+                          variant="outlined"
+                          size="large"
+                          onClick={() => secondaryButton.onClick()}
+                          disabled={secondaryButton.disabled}
+                        >
+                          {secondaryButton.title}
+                        </Button>
+                      ) : null}
+                    </Grid>
+                    <Grid item>
                       <Button
-                        variant="outlined"
+                        variant="contained"
                         size="large"
-                        onClick={() => secondaryButton.onClick()}
-                        disabled={secondaryButton.disabled}
+                        color="primary"
+                        onClick={() => primaryButton.onClick()}
+                        disabled={primaryButton.disabled}
                       >
-                        {secondaryButton.title}
+                        {primaryButton.title}
                       </Button>
-                    ) : null}
+                    </Grid>
                   </Grid>
-                  <Grid item>
-                    <Button
-                      variant="contained"
-                      size="large"
-                      color="primary"
-                      onClick={() => primaryButton.onClick()}
-                      disabled={primaryButton.disabled}
-                    >
-                      {primaryButton.title}
-                    </Button>
-                  </Grid>
-                </Grid>
-                <SWAPSpace size="middle" />
-              </Container>
-            </div>
-          </Paper>
+                  <SWAPSpace size="middle" />
+                </Container>
+              </div>
+            </Paper>
+          </ModalOpenEffect>
         </SWAPModalWrap>
       </Fade>
     </Modal>
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import SWAPLogo from "./SWAPLogo/SWAPLogo";
 import SWAPModal from "./SWAPModal/SWAPModal";
-import SWAPTheme from "./SWAPTheme/SWAPTheme";
 import SWAPSpace from "./SWAPSpace/SWAPSpace";
+import SWAPTheme from "./SWAPTheme/SWAPTheme";
 
-export { SWAPLogo, SWAPModal, SWAPTheme, SWAPSpace };
+export { SWAPLogo, SWAPModal, SWAPSpace, SWAPTheme };

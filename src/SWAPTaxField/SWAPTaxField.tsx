d58cd1e896bfbf1900c19449f9e93756b78535d7
Logan <clothe09986@gmail.com>
2021-08-19 18:44:17 +0800

    swap-ui 1.0.118: 更新執行業務所得資料
    更新執行業務所得資料

--- src/SWAPTaxField/SWAPTaxField.tsx
+++ src/SWAPTaxField/SWAPTaxField.tsx
@@ -467,7 +467,7 @@ const SWAPExpenseTypes: Array<{
   label: string;
   expenseRatio: string;
 }> = [
-  { label: "律師", expenseRatio: "30% ~ 50%", code: "10", source: "9A" },
+  { label: "律師", expenseRatio: "30%", code: "10", source: "9A" },
   { label: "會計師", expenseRatio: "30%", code: "11", source: "9A" },
   { label: "精算師", expenseRatio: "20%", code: "12", source: "9A" },
   { label: "地政士", expenseRatio: "30%", code: "13", source: "9A" },
@@ -499,6 +499,12 @@ const SWAPExpenseTypes: Array<{
   { label: "工匠（工資收入）", expenseRatio: "20%", code: "24", source: "9A" },
   { label: "工匠（工料收入）", expenseRatio: "62%", code: "25", source: "9A" },
   { label: "引水人", expenseRatio: "25%", code: "26", source: "9A" },
+  {
+    label: "律師配合政府政策辦理法律扶助案件之收入",
+    expenseRatio: "50%",
+    code: "27",
+    source: "9A",
+  },
   { label: "內科醫師", expenseRatio: "40%", code: "30", source: "9A" },
   { label: "外科醫師", expenseRatio: "45%", code: "31", source: "9A" },
   { label: "小兒科醫師", expenseRatio: "40%", code: "32", source: "9A" },
@@ -511,19 +517,17 @@ const SWAPExpenseTypes: Array<{
   { label: "其他科別醫師", expenseRatio: "43%", code: "39", source: "9A" },
   {
     label: "助產師（士）",
-    expenseRatio: "31% ~ 72%",
+    expenseRatio: "31%",
     code: "40",
     source: "9A",
   },
-  { label: "藥師", expenseRatio: "20% ~ 100%", code: "41", source: "9A" },
+  { label: "藥師", expenseRatio: "20%", code: "41", source: "9A" },
   {
     label: "醫事檢驗師（生）",
-    expenseRatio: "43% ~ 78%",
+    expenseRatio: "43%",
     code: "42",
     source: "9A",
   },
-  { label: "整合照護", expenseRatio: "0%", code: "43", source: "9A" },
-  { label: "駐診拆帳西醫", expenseRatio: "0%", code: "44", source: "9A" },
   { label: "營養師", expenseRatio: "20%", code: "45", source: "9A" },
   {
     label: "醫療機構醫師經核准前往他醫療機構從事醫療業務且不具僱傭關係者",
@@ -531,14 +535,14 @@ const SWAPExpenseTypes: Array<{
     code: "46",
     source: "9A",
   },
-  { label: "獸醫師", expenseRatio: "32% ~ 40%", code: "47", source: "9A" },
+  { label: "獸醫師", expenseRatio: "32%", code: "47", source: "9A" },
   { label: "皮膚科醫師", expenseRatio: "40%", code: "48", source: "9A" },
   { label: "家庭醫學科醫師", expenseRatio: "40%", code: "49", source: "9A" },
-  { label: "中醫師", expenseRatio: "20% ~ 78%", code: "50", source: "9A" },
-  { label: "語言治療師", expenseRatio: "20% ~ 78%", code: "51", source: "9A" },
+  { label: "中醫師", expenseRatio: "20%", code: "50", source: "9A" },
+  { label: "語言治療師", expenseRatio: "20%", code: "51", source: "9A" },
   { label: "人壽保險醫療檢查", expenseRatio: "35%", code: "52", source: "9A" },
-  { label: "物理治療師", expenseRatio: "43% ~ 78%", code: "53", source: "9A" },
-  { label: "職能治療師", expenseRatio: "43% ~ 78%", code: "54", source: "9A" },
+  { label: "物理治療師", expenseRatio: "43%", code: "53", source: "9A" },
+  { label: "職能治療師", expenseRatio: "43%", code: "54", source: "9A" },
   { label: "心理師", expenseRatio: "20%", code: "55", source: "9A" },
   { label: "牙體技術師", expenseRatio: "40%", code: "56", source: "9A" },
   {
@@ -581,7 +585,7 @@ const SWAPExpenseTypes: Array<{
   { label: "其他", expenseRatio: "0%", code: "90", source: "9A" },
   { label: "商標代理人", expenseRatio: "30%", code: "91", source: "9A" },
   { label: "程式設計師", expenseRatio: "20%", code: "92", source: "9A" },
-  { label: "專利代理人", expenseRatio: "30%", code: "93", source: "9A" },
+  { label: "專利師及專利代理", expenseRatio: "30%", code: "93", source: "9A" },
   {
     label: "未具律師資格，辦理訴訟代理人業務",
     expenseRatio: "23%",

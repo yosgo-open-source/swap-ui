ad1fab6a45a5984cf4ff04be26d04f9fd9c71fd1
陳貝綸 <kcin1993@gmail.com>
2026-07-16 17:33:26 +0800

    build: react-share 5.3.0 / @mui/x-date-pickers 9.9.0 / dayjs 1.11.21 依賴 + SVG 資產支援

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,8 @@
 // @fontsource 等 CSS side-effect import 的型別宣告
 declare module "*.css";
+
+// SVG 資產（vite 回傳 URL、tsup dataurl 回傳 base64 字串，皆可作 img src）
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -14,4 +14,5 @@ export default defineConfig({
     "@emotion/react",
     "@emotion/styled",
   ],
+  loader: { ".svg": "dataurl" },
 });

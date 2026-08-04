1ee1dba505ea8dcf3b016c5715412e2593259ac0
Kcin1993 <Kcin1993@users.noreply.github.com>
2026-08-04 15:53:14 +0000

    deploy: v2 Storybook（CI 自動部署） 2f93e30151b3fbc05ddfba123a7c6c147d95f29e

---
+++ v2/assets/SWAPShare-8Z5mEhfJ.js
@@ -0,0 +1,15 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-cM__dR4X.js";import{i as n}from"./react-XnqUzw--.js";import{c as r,l as i,n as a,s as o}from"./blocks-C0XwP2PW.js";import{t as s}from"./mdx-react-shim-y1jXGhTh.js";import{Basic as c,n as l,t as u}from"./SWAPShare.stories-Dn63mtyF.js";function d(e){let t={code:`code`,h1:`h1`,h2:`h2`,li:`li`,p:`p`,ul:`ul`,...n(),...e.components};return(0,p.jsxs)(p.Fragment,{children:[(0,p.jsx)(o,{of:u}),`
+`,(0,p.jsx)(t.h1,{id:`swapshare`,children:`SWAPShare`}),`
+`,(0,p.jsx)(t.p,{children:`一列社群分享按鈕：Messenger、LINE、Email。`}),`
+`,(0,p.jsx)(t.h2,{id:`使用時機`,children:`使用時機`}),`
+`,(0,p.jsxs)(t.ul,{children:[`
+`,(0,p.jsx)(t.li,{children:`分享請款單、個人頁等公開連結。`}),`
+`,(0,p.jsxs)(t.li,{children:[(0,p.jsx)(t.code,{children:`url`}),` 留空時分享當前頁網址；`,(0,p.jsx)(t.code,{children:`emailSubject`}),` / `,(0,p.jsx)(t.code,{children:`sharedContent`}),` 帶入 Email 主旨與內文。`]}),`
+`,(0,p.jsx)(t.li,{children:`Messenger 在行動裝置走 App 深連結、桌面走網頁對話框（沿 v1 行為）。`}),`
+`]}),`
+`,(0,p.jsx)(t.h2,{id:`基本用法`,children:`基本用法`}),`
+`,(0,p.jsx)(r,{language:`tsx`,code:`import { SWAPShare } from "@yosgo/swap-ui";
+
+<SWAPShare url={publicUrl} emailSubject="推薦你看看" sharedContent="說明文字" />`}),`
+`,(0,p.jsx)(t.h2,{id:`基本`,children:`基本`}),`
+`,(0,p.jsx)(a,{of:c})]})}function f(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,p.jsx)(t,{...e,children:(0,p.jsx)(d,{...e})}):d(e)}var p;e((()=>{p=t(),s(),i(),l()}))();export{f as default};

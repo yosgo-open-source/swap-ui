223b2ca7e5caab0d26b48951070b3c8ae778d9fb
Kcin1993 <Kcin1993@users.noreply.github.com>
2026-08-04 13:11:28 +0000

    deploy: v2 Storybook（CI 自動部署） da1bbe249c246166b2fc0f86256f5f871e64a333

---
+++ v2/assets/Breadcrumb-DvTW07Pm.js
@@ -0,0 +1,21 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-cM__dR4X.js";import{i as n}from"./react-XnqUzw--.js";import{c as r,l as i,n as a,s as o}from"./blocks-xw-MS0Oi.js";import{t as s}from"./mdx-react-shim-y1jXGhTh.js";import{Basic as c,Collapsed as l,n as u,t as d}from"./Breadcrumb.stories-CkKTUh9X.js";function f(e){let t={code:`code`,h1:`h1`,h2:`h2`,li:`li`,p:`p`,ul:`ul`,...n(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(o,{of:d}),`
+`,(0,m.jsx)(t.h1,{id:`breadcrumb`,children:`Breadcrumb`}),`
+`,(0,m.jsxs)(t.p,{children:[`麵包屑導覽，標示當前頁在層級中的位置。最後一項用 `,(0,m.jsx)(t.code,{children:`last`}),`（黑色粗體、不可點）。`]}),`
+`,(0,m.jsx)(t.h2,{id:`使用時機`,children:`使用時機`}),`
+`,(0,m.jsxs)(t.ul,{children:[`
+`,(0,m.jsx)(t.li,{children:`層級三層以上的頁面（列表 → 分類 → 詳情），幫使用者快速回上層。`}),`
+`,(0,m.jsxs)(t.li,{children:[`層級太深時給 `,(0,m.jsx)(t.code,{children:`maxItems`}),`，中間項會收合成「⋯」選單。`]}),`
+`]}),`
+`,(0,m.jsx)(t.h2,{id:`基本用法`,children:`基本用法`}),`
+`,(0,m.jsx)(r,{language:`tsx`,code:`import { Breadcrumb, BreadcrumbItem } from "@yosgo/swap-ui";
+
+<Breadcrumb>
+{[
+  <BreadcrumbItem key="1" href="/">首頁</BreadcrumbItem>,
+  <BreadcrumbItem key="2" last>目前頁面</BreadcrumbItem>,
+]}
+</Breadcrumb>`}),`
+`,(0,m.jsx)(t.h2,{id:`基本`,children:`基本`}),`
+`,(0,m.jsx)(a,{of:c}),`
+`,(0,m.jsx)(t.h2,{id:`收合maxitems`,children:`收合（maxItems）`}),`
+`,(0,m.jsx)(a,{of:l})]})}function p(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=t(),s(),i(),u()}))();export{p as default};
---
+++ v2/assets/SWAPLogo-DgvuVqFI.js
@@ -0,0 +1,16 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{t}from"./jsx-runtime-cM__dR4X.js";import{i as n}from"./react-XnqUzw--.js";import{c as r,l as i,n as a,s as o}from"./blocks-xw-MS0Oi.js";import{t as s}from"./mdx-react-shim-y1jXGhTh.js";import{Variants as c,n as l,t as u}from"./SWAPLogo.stories-DfXfr399.js";function d(e){let t={code:`code`,h1:`h1`,h2:`h2`,li:`li`,p:`p`,ul:`ul`,...n(),...e.components};return(0,p.jsxs)(p.Fragment,{children:[(0,p.jsx)(o,{of:u}),`
+`,(0,p.jsx)(t.h1,{id:`swaplogo`,children:`SWAPLogo`}),`
+`,(0,p.jsx)(t.p,{children:`SWAP 品牌 logo（8 種資產：一般/黑白/純圖示/BIZ 英中/各深色版）。`}),`
+`,(0,p.jsx)(t.h2,{id:`使用時機`,children:`使用時機`}),`
+`,(0,p.jsxs)(t.ul,{children:[`
+`,(0,p.jsx)(t.li,{children:`導覽列、頁尾、登入頁等品牌露出。`}),`
+`,(0,p.jsxs)(t.li,{children:[`深色背景加 `,(0,p.jsx)(t.code,{children:`dark`}),`；BIZ 產品線用 `,(0,p.jsx)(t.code,{children:`business`}),`（中文版加 `,(0,p.jsx)(t.code,{children:`chinese`}),`）。`]}),`
+`,(0,p.jsxs)(t.li,{children:[(0,p.jsx)(t.code,{children:`size`}),`：small 30 / middle 40 / large 48（BIZ 為 48/72/81），也可給數字或 CSS 長度。`]}),`
+`]}),`
+`,(0,p.jsx)(t.h2,{id:`基本用法`,children:`基本用法`}),`
+`,(0,p.jsx)(r,{language:`tsx`,code:`import { SWAPLogo } from "@yosgo/swap-ui";
+
+<SWAPLogo size="small" />
+<SWAPLogo business chinese />`}),`
+`,(0,p.jsx)(t.h2,{id:`全部變體`,children:`全部變體`}),`
+`,(0,p.jsx)(a,{of:c})]})}function f(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,p.jsx)(t,{...e,children:(0,p.jsx)(d,{...e})}):d(e)}var p;e((()=>{p=t(),s(),i(),l()}))();export{f as default};
